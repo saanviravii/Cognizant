24ec4ab0d2b16e76ef661874b54e5fa3e0294ae8
saanviravii <142113935+saanviravii@users.noreply.github.com>
2023-08-15 14:58:52 +0530

    Update login.js

--- login.js
+++ login.js
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
-import React, { useEffect, useState } from "react";
+import React, { useState, useEffect } from "react";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
@@ -10,21 +10,23 @@ function Login({ user }) {
     const [loading, setloading] = useState(false);
     const [account, setaccount] = useState("");
     const router = useRouter();
-      useEffect(() => {
+
+    useEffect(() => {
         if (localStorage.getItem("jwt")) {
-          router.push("/");
+            router.push("/");
         }
-      }, []);
+    }, []);
+
     const change = (e) => {
-        if (e.target.name == "email") {
+        if (e.target.name === "email") {
             setemail(e.target.value);
-        } else if (e.target.name == "password") {
+        } else if (e.target.name === "password") {
             setpassword(e.target.value);
-        }
-        else if (e.target.name == "account") {
+        } else if (e.target.name === "account") {
             setaccount(e.target.value);
         }
     };
+
     const onSubmit = async (e) => {
         e.preventDefault();
         setloading(true);
@@ -35,15 +37,15 @@ function Login({ user }) {
         let res = await fetch(`http://localhost:1337/api/auth/local/`, {
             method: "POST",
             headers: {
-
                 "Content-Type": "application/json",
             },
             body: JSON.stringify(data1),
         });
         const data = await res.json();
         setloading(false);
+
         if (!data.error) {
-            toast.success("login successfull!", {
+            toast.success("login successful!", {
                 position: "bottom-center",
                 autoClose: 3000,
                 hideProgressBar: false,
@@ -53,8 +55,13 @@ function Login({ user }) {
                 progress: undefined,
                 theme: "light",
             });
-            localStorage.setItem("jwt",data.jwt);
-            router.push("/");
+            localStorage.setItem("jwt", data.jwt);
+
+            if (account === "admin") {
+                router.push("/");
+            } else if (account === "user") {
+                router.push("/vendor");
+            }
         } else {
             toast.error(data.error.message, {
                 position: "bottom-center",
@@ -70,8 +77,6 @@ function Login({ user }) {
     };
 
     return (
-
-
         <div>
             <ToastContainer />
             <form
@@ -82,23 +87,27 @@ function Login({ user }) {
                     Sign in to your account
                 </h1>
                 <div className="mb-3">
-
-                    <label for="account" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Select an option</label>
-                    <select onChange={change}
+                    <label
+                        htmlFor="account"
+                        className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                    >
+                        Select an option
+                    </label>
+                    <select
+                        onChange={change}
                         type="text"
-
                         name="account"
                         value={account || ""}
-                        id="account" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 my-2">
-
-                        <option selected>Choose Admin/User</option>
+                        id="account"
+                        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 my-2"
+                    >
+                        <option value="" disabled>Choose Admin/User</option>
                         <option value="admin">Admin</option>
                         <option value="user">User</option>
-
                     </select>
                     <label
                         htmlFor="email"
-                        className="block mb-2 text-sm font-medium text-gray-900 "
+                        className="block mb-2 text-sm font-medium text-gray-900"
                     >
                         Your email
                     </label>
@@ -108,7 +117,7 @@ function Login({ user }) {
                         id="email"
                         value={email}
                         onChange={change}
-                        className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-orange-500 focus:border-orange-500 block w-full p-2.5 "
+                        className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-orange-500 focus:border-orange-500 block w-full p-2.5"
                         placeholder="name@gmail.com"
                         required
                     />
@@ -116,19 +125,17 @@ function Login({ user }) {
                 <div className="mb-3">
                     <label
                         htmlFor="password"
-                        className="block mb-2 text-sm font-medium text-gray-900 "
+                        className="block mb-2 text-sm font-medium text-gray-900"
                     >
                         Your password
                     </label>
-
                     <input
                         type="password"
                         id="password"
                         name="password"
                         value={password}
-                        min={6}
                         onChange={change}
-                        className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-orange-500 focus:border-orange-500 block w-full p-2.5 "
+                        className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-orange-500 focus:border-orange-500 block w-full p-2.5"
                         placeholder="Password"
                         required
                     />
@@ -136,33 +143,24 @@ function Login({ user }) {
                 {!loading ? (
                     <button
                         type="submit"
-                        className="text-white mt-2 bg-orange-500 hover:bg-orange-600 focus:ring-4 focus:outline-none focus:ring-orange-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center "
+                        className="text-white mt-2 bg-orange-500 hover:bg-orange-600 focus:ring-4 focus:outline-none focus:ring-orange-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
                     >
                         Sign in to account
                     </button>
                 ) : (
                     <button
                         type="submit"
-                        className="text-white bg-orange-500 hover:bg-orange-600 focus:ring-4 focus:outline-none focus:ring-orange-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center
-             cursor-not-allowed
-            "
+                        className="text-white bg-orange-500 hover:bg-orange-600 focus:ring-4 focus:outline-none focus:ring-orange-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center cursor-not-allowed"
                     >
                         <div className="flex" role="status">
                             <svg
                                 aria-hidden="true"
-                                className="inline w-8 h-6 mr-1 text-gray-200 animate-spin  fill-yellow-400"
+                                className="inline w-8 h-6 mr-1 text-gray-200 animate-spin fill-yellow-400"
                                 viewBox="0 0 100 101"
                                 fill="none"
                                 xmlns="http://www.w3.org/2000/svg"
                             >
-                                <path
-                                    d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
-                                    fill="currentColor"
-                                />
-                                <path
-                                    d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
-                                    fill="currentFill"
-                                />
+                                {/* ... (animated spinner path data) */}
                             </svg>
                             <span className="sr-only">Loading...</span>
                             <p>Loading....</p>
@@ -177,7 +175,6 @@ function Login({ user }) {
                     >
                         <p>&nbsp; Create an account.</p>
                     </Link>
-
                 </div>
             </form>
         </div>
